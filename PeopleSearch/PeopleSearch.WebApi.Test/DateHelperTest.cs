9deb884e908c106905ce4f91e66e902ff6e68f6f
Matthew Paul <mpaul@captechconsulting.com>
2017-11-10 16:29:40 -0500

    Added Date Helper Unit Tests

---
+++ PeopleSearch/PeopleSearch.WebApi.Test/DateHelperTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PeopleSearch.WebApi.Common;
+
+namespace PeopleSearch.WebApi.Test
+{
+    [TestClass]
+    public class DateHelperTest
+    {
+        [TestMethod]
+        public void NotPastInCurrentYearDayTest_Test()
+        {
+            // Set up test
+            var birthDate = new DateTime(2016, 6, 30);
+            var currentDate = new DateTime(2018, 5, 15);
+
+            // Execute Test
+            var result = DateHelper.AgeInYears(birthDate, currentDate);
+
+            // Verify
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void NotPastInCurrentYear_Test()
+        {
+            // Set up test
+            var birthDate = new DateTime(2016, 6, 1);
+            var currentDate = new DateTime(2017, 5, 1);
+
+            // Execute Test
+            var result = DateHelper.AgeInYears(birthDate, currentDate);
+
+            // Verify
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void PastInCurrentYear_Test()
+        {
+            // Set up test
+            var birthDate = new DateTime(2016, 6, 1);
+            var currentDate = new DateTime(2017, 7, 1);
+
+            // Execute Test
+            var result = DateHelper.AgeInYears(birthDate, currentDate);
+
+            // Verify
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void NotPastLeapYear_Test()
+        {
+            // Set up test
+            var birthDate = new DateTime(2016, 2, 29);
+            var currentDate = new DateTime(2024, 2, 1);
+
+            // Execute Test
+            var result = DateHelper.AgeInYears(birthDate, currentDate);
+
+            // Verify
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void PastLeapYear_Test()
+        {
+            // Set up test
+            var birthDate = new DateTime(2016, 2, 29);
+            var currentDate = new DateTime(2024, 3, 1);
+
+            // Execute Test
+            var result = DateHelper.AgeInYears(birthDate, currentDate);
+
+            // Verify
+            Assert.AreEqual(8, result);
+        }
+    }
+}
